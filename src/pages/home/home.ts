80224b889ffdbee5b4989e7a0b1d2fc3bb10fd6d
aamir-shah <syedaamir974@gmail.com>
2016-11-17 19:26:15 +0500

    initial setup for cordova-plugin-IMEI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+    constructor(public navCtrl: NavController, private platform: Platform) {
+        platform.ready().then((readySource) => {
+            console.log('Platform ready from', readySource);
+            // Platform now ready, execute any required native code
+
+            (<any>window).plugins.imei.get(
+                function(imei) {
+                    console.log("got imei: " + imei);
+                },
+                function() {
+                    console.log("error loading imei");
+                }
+            );
+
+        });
+    }
 
 }
